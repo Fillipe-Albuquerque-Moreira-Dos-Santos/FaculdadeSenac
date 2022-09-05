1bfa39350e38281227eba83548d1e27ad1b494e8
Fillipe Albuquerque Moreira Dos Santos <03358469123@edu.senac.br>
2022-09-05 11:36:56 -0300

    Vetor

---
+++ vetor/programa1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+   /*
+     echo '<h1>for</h1>';
+     for ($i=0; $i < 100; $i++) {   
+            echo "<p>[$i]</p>";   
+     }
+     */
+/*
+     $nota ['Fillipe'] = 7;
+     $nota ['Guilherme'] = 8.5;
+     $nota['Deibson'] = 9.8;
+     
+     foreach ($nota as $p=>$n) {
+         echo "Aluno $p Nota: $n<br>";
+     }
+     */
+
+     $frutas = array ('Maça', 'Laranka', 'Pera', 'Caqui');
+     $verduras = ['Alface', 'Couve', 'Acelga'];
+
+     $alimentos = array ($Frutas, $verduras);
+     
+    ?>
+</body>
+</html>
